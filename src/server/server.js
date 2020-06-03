1d642d31eb989dc820405e7f6d3479fb9c5bf321
Darren Jensen <darren.jensen@gmail.com>
2020-06-03 17:48:18 +0700

    first commit

---
+++ src/server/server.js
@@ -0,0 +1,35 @@
+import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import Config from './config.json';
+import Web3 from 'web3';
+import express from 'express';
+
+
+let config = Config['localhost'];
+let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+web3.eth.defaultAccount = web3.eth.accounts[0];
+let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
+console.log('The App Contract Address: ', config.appAddress);
+console.log('Web3 Version', web3.version);
+
+flightSuretyApp.events.OracleRequest({
+    fromBlock: 0
+  }, function (error, event) {
+    if (error) console.log(error)
+    console.log(event)
+});
+
+// flightSuretyApp.getPastEvents('OracleRequest',
+//   {fromBlock: 0}, function (error, event) {
+//       if (error) console.log('************ ERROR: ', error)
+//       console.log(event)
+//   })
+
+const app = express();
+app.get('/api', (req, res) => {
+    res.send({
+      message: 'An API for use with your Dapp!'
+    })
+})
+
+export default app;
