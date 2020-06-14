79541aa341cb67115149cc0e39cde79220b25bbb
Darren Jensen <darren.jensen@gmail.com>
2020-06-14 19:49:34 +0700

    corrected ganache settings for travis ci

--- bin/test.sh
+++ bin/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ganache-cli -p 8545 --gasLimit 10000000 2> /dev/null 1> /dev/null &
+ganache-cli -a 50 -p 8545 --gasLimit 300000000 --gasPrice 20000000000 -m 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat' 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
 truffle compile
